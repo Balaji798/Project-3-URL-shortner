7f64c3f514303e912aedff99e2e37ddcebd449ec
Balaji798 <93571474+Balaji798@users.noreply.github.com>
2022-01-24 18:57:29 +0530

    Update urlControllers.js

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -1,4 +1,3 @@
-const validUrl=require('valid-url')
 const shortid= require('shortid')
 
 const urlModal=require("../modal/urlModel")
@@ -45,7 +44,7 @@ const creatShortedUrl=async function (req, res){
         return res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide URL' })
     }
 
-    const longUrl = req.body.longUrl.trim()
+    const longUrl = req.body.longUrl.trim().toLowerCase()
     
     if(!(/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(longUrl))){
       res.status(400).send({staus:false,message:"longUrl is not valid"})
@@ -55,8 +54,9 @@ const creatShortedUrl=async function (req, res){
     let urlCode = shortid.generate().match(/[a-z\A-Z]/g).join("")     //this will give only Alphabet
     urlCode=urlCode.toLowerCase()
 
-    let url = await urlModal.find({ longUrl })
-    if (url>0) {
+    let url = await urlModal.findOne({ longUrl:longUrl })
+  
+    if (url) {
        return res.status(200).send({ status: true, data: url }) //if already exist
     }
     //if new longUrl is there
